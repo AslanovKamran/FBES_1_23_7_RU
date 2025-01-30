9e1d862b269a37e2c89d78c6eb81d63621d360b4
Kamran Aslanov <kamran.aslanov@do-it.az>
2025-01-30 10:04:51 +0400

    First Web App Init

---
+++ FirstWebApp(29-01-2025)/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp_29_01_2025_.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Contacts()
+    {
+        ViewBag.Contacts = new List<string>
+        {
+            "John", "Bob", "Cody","Max"
+        };
+        return View();
+    }
+}
---
+++ FirstWebApp(29-01-2025)/Program.cs
@@ -0,0 +1,18 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+//"Учим" наше приложение использовать Controllers и Views
+//Через builder.Services в наш проект добавляются зависимости напр связь с БД 
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+//Добавляем middleware, который будет уметь работать со статическими файлами
+//Напр. фотографии, стили, favicon и тп.
+app.UseStaticFiles();
+
+//Задаем нашему приложению путь по умолчанию (при необходимости его можно менять)
+//Таким образом при открытии браузера в URL будет /Home/Index
+//Где Home - имя контроллера (раздела майта), a Index - имя метода (страницы)
+app.MapDefaultControllerRoute();
+
+app.Run();
