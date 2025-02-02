1b28d478523bc68e14e1c4df58f07a2f1e834298
Aslan17 <k.aslan1707@gmail.com>
2025-02-02 04:49:35 +0400

    EF Core Added

--- AspNetDBPortal/Controllers/HomeController.cs
+++ AspNetDBPortal/Controllers/HomeController.cs
@@ -11,9 +11,8 @@ namespace AspNetDBPortal.Controllers
 
         public IActionResult Index()
         {
-            ViewBag.Posts = _context.Posts.ToList();
-
-            return View();
+            var posts = _context.Posts.ToList();
+            return View(posts);
         }
 
         [HttpGet]
@@ -39,6 +38,28 @@ namespace AspNetDBPortal.Controllers
                 TempData["Exception"] = ex.Message;
             }
 
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int id)
+        {
+            var posts = _context?.Posts.ToList();
+            var post = posts?.FirstOrDefault(p => p.Id == id);
+
+            return View(post);
+        }
+
+        [HttpPost]
+        public IActionResult DeletePost(int id)
+        {
+            var posts = _context.Posts.ToList();
+            var postToDelete = posts.FirstOrDefault(p => p.Id == id);
+            if (postToDelete != null)
+            {
+                _context.Posts.Remove(postToDelete);
+                _context.SaveChanges();
+                TempData["Deleted"] = "Post has been deleted!";
+            }
             return RedirectToAction(nameof(Index));
         }
 
--- AspNetDBPortal/Models/Post.cs
+++ AspNetDBPortal/Models/Post.cs
@@ -6,14 +6,20 @@ public class Post
 {
     [Key]
     public int Id { get; set; }
-    [Required(AllowEmptyStrings = false)]
-    [MaxLength(100)]
+    
+    [Required(AllowEmptyStrings = false,ErrorMessage ="Zapolni title")]
+    [MaxLength(100, ErrorMessage ="Ochen bolshoi title")]
+    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage ="Tolko bukvi plz")]
     public string Title { get; set; } = string.Empty;
+
     [Required(AllowEmptyStrings = false)]
     [MaxLength(200)]
     public string Content { get; set; } = string.Empty;
+    
     public string? Description { get; set; } = string.Empty;
+    
     public string? ImageUrl { get; set; } = string.Empty;
+    
     public DateTime CreatedAt { get; set; }
 
     //Auto set
