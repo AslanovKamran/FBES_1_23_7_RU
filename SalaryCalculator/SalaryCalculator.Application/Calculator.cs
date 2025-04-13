b71b355aa2fada4daa1215f2c2075a7465e1bfbe
Aslan17 <k.aslan1707@gmail.com>
2025-04-14 01:46:44 +0400

    xUnit Added

---
+++ SalaryCalculator/SalaryCalculator.Application/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace SalaryCalculator.Application;
+
+public static class Calculator
+{
+    public static double Add(double x, double y) => x + y;
+
+    public static double Subtract(double x, double y) => x - y;
+
+    public static double Multiply(double x, double y) => x * y;
+
+    public static double Divide(double x, double y)
+    {
+        return y == 0 ? 0 : x / y;
+    }
+}
---
+++ SalaryCalculator/SalaryCalculator.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using SalaryCalculator.Application;
+using SalaryCalculator.Application.Models;
+
+var newPerson = new PersonModel()
+{
+    FirstName = "Josh",
+    LastName = "Peck"
+};
+
+DataAccess.AddNewPerson(newPerson);
+var people = DataAccess.GetAllPeople();
+foreach (var item in people)
+{
+    Console.WriteLine(item.FullName);
+}
